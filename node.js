f3d6108066b0289290e69ae35c37533cefaa57e4
Mohit Gupta <mohitgupta20062212@gmail.com>
2026-09-08 11:40:36 +0530

    first commit

---
+++ node.js
@@ -0,0 +1,9 @@
+function sum(... values){
+    let sum = 0;
+    for(let i in values){
+        sum = sum + values[i];
+    }
+    console.log(sum);
+};
+let arr =[10,20,30,40];
+sum(...arr);
---
+++ shopping.js
@@ -0,0 +1,2 @@
+console.log("shopping list");
+ n
